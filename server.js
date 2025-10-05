35ccf4ea4e14d42d0704ad9f0e0dd642fbf0a674
anu882882 <anubsr882@gmail.com>
2025-10-05 13:27:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,4 +60,4 @@ app.post('/api/send', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('App ready at http://localhost:3000'));
+app.listen(process.env.PORT || 3000, () => console.log('App running on port', process.env.PORT || 3000));
